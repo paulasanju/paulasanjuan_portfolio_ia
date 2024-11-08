49910295dced87eed04af645772e61c2e737b14d
paulasanju <114092890+paulasanju@users.noreply.github.com>
2024-11-08 20:40:10 +0100

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -2,18 +2,18 @@ import streamlit as st
 
 # Defino las paginas
 profile = st.Page(
-    "home/0_Sobre_mi.py",
+    "home/Sobre_mi.py",
     title="Sobre mi",
     icon=":material/description:",
     default=True,
 )
 copilot = st.Page(
-    "projects/1_Copilot_Resumenes.py",
+    "projects/Copilot_Resumenes.py",
     title="Copilot Generación de Resúmenes",
     icon=":material/precision_manufacturing:",
 )
 chatbot = st.Page(
-    "projects/2_Chatbot_Q&A.py",
+    "projects/Chatbot_Q&A.py",
     title="Chatbot de Preguntas y Respuestas",
     icon=":material/smart_toy:",
 )
