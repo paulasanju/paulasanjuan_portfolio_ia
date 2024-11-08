3e51a56569bf81d4b30ce55de5d34e2e778c0b38
paulasanju <114092890+paulasanju@users.noreply.github.com>
2024-11-08 20:31:28 +0100

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
-st.write('Hello world!')
+# Define the pages
+profile = st.Page(
+    "home/0_Sobre_mi.py",
+    title="Sobre mi",
+    icon=":material/description:",
+    default=True,
+)
+copilot = st.Page(
+    "projects/1_Copilot_Resumenes.py",
+    title="Copilot Generación de Resúmenes",
+    icon=":material/precision_manufacturing:",
+)
+chatbot = st.Page(
+    "projects/2_Chatbot_Q&A.py",
+    title="Chatbot de Preguntas y Respuestas",
+    icon=":material/smart_toy:",
+)
+
+# Define the navigation panel
+pg = st.navigation(
+    {
+        "Home": [profile],
+        "Proyectos": [copilot, chatbot],
+    }
+)
+
+# Start the app
+pg.run()
